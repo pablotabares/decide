74d84db19008b7ea518b15ff84e4fac71b064579
Daniel <danielcarpioc@gmail.com>
2018-11-20 19:50:46 +0100

    Added weighted random selection postprocessing method
    Added a new method to postprocessing votes #8

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import random
 
 
 class PostProcView(APIView):
@@ -28,6 +29,33 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+    def weightedRandomSelection(self, options):
+        print(options)
+        out = []
+        nVotes = 0
+        for opt in options:
+            nVotes += opt["votes"]
+        randomValue = random.randint(0, nVotes-1)
+        print("Random Value: " + str(randomValue))
+        found = False
+        for i in range(0, len(options)):
+            randomValue -= options[i]["votes"]
+            if randomValue < 0 and not found:
+                out.append({
+                    **options[i],
+                    'postproc': 1
+                })
+                found = True
+            else:
+                out.append({
+                    **options[i],
+                    'postproc': 0
+                })
+        out.sort(key=lambda x: -x['postproc'])
+        print(str(out))
+        return Response(out)
+
+
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -48,5 +76,7 @@ class PostProcView(APIView):
             return self.identity(opts)
         elif t =='WEIGHT':
             return self.weight(opts)
+        elif t == 'WEIGHTED-RANDOM':
+            return self.weightedRandomSelection(opts)
 
         return Response({})
