078674af061435f4f32b6b057d710c609004d28f
Unknown <cami_jara@hotmail.es>
2018-12-02 18:48:54 +0100

    Bux fixed
    Entry point added inside Conversation Handler function [#33]

--- bot/poll.py
+++ bot/poll.py
@@ -7,13 +7,13 @@ DESCRIPTION, QUESTION = range(2)
 def name(bot,update):
      update.message.reply_text('Vas a crear una nueva votación. En primer lugar, indica el nombre de la misma :)')
 
-     return ConversationHandler.DESCRIPTION
+     return DESCRIPTION
 
 def description(bot,update):
 #     logger.info("Name of poll: %s", update.message.text)
      update.message.reply_text('¡De acuerdo! A continuación, indica la descripción.')
 
-     return ConversationHandler.QUESTION
+     return QUESTION
 
 def question(bot,update):
 #     logger.info("Description of poll: %s", update.message.text)
@@ -31,6 +31,8 @@ def main(dispatcher):
      # start_handler = CommandHandler('poll', poll)
      # dispatcher.add_handler(start_handler)
      conv_handler = ConversationHandler(
+     entry_points=[CommandHandler('name', name)],
+
      states={
           DESCRIPTION: [MessageHandler(Filters.text, description)],
 
@@ -40,6 +42,3 @@ def main(dispatcher):
      fallbacks=[CommandHandler('cancel', cancel)]
      )
      dispatcher.add_handler(conv_handler)
-
-     start_handler = CommandHandler('name', name)
-     dispatcher.add_handler(start_handler)
