98b2006e2e6b27710d4310e783fe5c6714c10ea3
Unknown <cami_jara@hotmail.es>
2018-12-02 18:35:37 +0100

    Conversation Handler added
    Conversation Handler has been added in order to guide the user in the process of poll creation [#33]

---
+++ bot/poll.py
@@ -0,0 +1,44 @@
+# encoding: utf-8
+from telegram.ext import (CommandHandler, Filters, ConversationHandler, MessageHandler)
+# import utils.logger as logger
+
+DESCRIPTION, QUESTION = range(2)
+
+def name(bot,update):
+     update.message.reply_text('Vas a crear una nueva votación. En primer lugar, indica el nombre de la misma :)')
+
+     return ConversationHandler.DESCRIPTION
+
+def description(bot,update):
+#     logger.info("Name of poll: %s", update.message.text)
+     update.message.reply_text('¡De acuerdo! A continuación, indica la descripción.')
+
+     return ConversationHandler.QUESTION
+
+def question(bot,update):
+#     logger.info("Description of poll: %s", update.message.text)
+     update.message.reply_text('Todo listo, ¡nos vemos!')
+
+     return ConversationHandler.END
+	
+def cancel(bot, update):
+     user = update.message.from_user
+     update.message.reply_text('La creación de la votación se ha cancelado :(')
+
+     return ConversationHandler.END
+
+def main(dispatcher):
+     # start_handler = CommandHandler('poll', poll)
+     # dispatcher.add_handler(start_handler)
+     conv_handler = ConversationHandler(
+     entry_points=[CommandHandler('name', name)],
+
+     states={
+          DESCRIPTION: [MessageHandler(Filters.text, description)],
+
+          QUESTION: [MessageHandler(Filters.text, question)],
+     },
+
+     fallbacks=[CommandHandler('cancel', cancel)]
+     )
+     dispatcher.add_handler(conv_handler)
--- bot/start.py
+++ bot/start.py
@@ -6,4 +6,7 @@ def main(dispatcher):
     dispatcher.add_handler(start_handler)
     
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Soy el bot de Decide-Ortosia, ¡háblame!")
+    bot.send_message(chat_id=update.message.chat_id, text="""Soy el bot de Decide-Ortosia :)
+                                                            Puedes usar los siguientes comandos:
+                                                            - /login para iniciar sesión
+                                                            - /poll para crear una nueva votación""")
