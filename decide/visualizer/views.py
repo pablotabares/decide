15582865df2e7dab3e1b281586eb47747493dae6
Luis Gil <luigilgue@alum.us.es>
2018-12-05 19:38:09 +0100

    Visualizer - Added the info of census to context.
    Description: I added an array to context with the data of census for that voting.
    Issue: #48

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.http import Http404
 
 from base import mods
+from census.models import Census
+from voting.models import Voting
 
 class VisualizerView(TemplateView):
     template_name = 'visualizer/visualizer.html'
@@ -10,11 +12,15 @@ class VisualizerView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
-
         try:
             r = mods.get('voting', params={'id': vid})
+            c = mods.get('census', params={'voting_id': vid}, HTTP_AUTHORIZATION='Token 401d04798f526bb074e51b15fa5a278d933b4108') 
+            #Investigar otra forma de pasar Token
             context['voting'] = r[0]
+            context['census'] = c
+            #print(context)
         except:
             raise Http404
 
         return context
+
