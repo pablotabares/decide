1c63478f4ba8310406437e7d8f5d97df7a74f46c
Unknown <cami_jara@hotmail.es>
2018-12-06 11:53:53 +0100

    Poll function implemented
    Bot now can handle a conversation and create a voting object [close #33]

--- bot/poll.py
+++ bot/poll.py
@@ -1,45 +1,91 @@
 # encoding: utf-8
 from telegram.ext import (CommandHandler, Filters, ConversationHandler, MessageHandler)
-# import utils.logger as logger
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+import utils.logger as logger
+from test.test_enum import Question
 
-DESCRIPTION, QUESTION = range(2)
+NAME, DESCRIPTION, QUESTION, ANSWERS, NEXT_QUESTION = range(5)
 
-def name(bot,update):
+VOTING = {}
+
+def poll(bot,update):
+     logger.get_logger().info("Name of poll: %s", update.message.text)
      update.message.reply_text('Vas a crear una nueva votación. En primer lugar, indica el nombre de la misma :)')
+
+     return NAME
+
+def set_name(bot,update):
+     global VOTING
+     VOTING['name'] = update.message.text
+
+     logger.get_logger().info("Name of poll: %s", update.message.text)
+     update.message.reply_text('¡De acuerdo! A continuación, indica la descripción.')
 
      return DESCRIPTION
 
-def description(bot,update):
-#     logger.info("Name of poll: %s", update.message.text)
-     update.message.reply_text('¡De acuerdo! A continuación, indica la descripción.')
+def set_description(bot,update):
+     global VOTING
+     VOTING['desc'] = update.message.text
+     logger.get_logger().info("Description of poll: %s", update.message.text)
+     update.message.reply_text('Ahora tienes que añadir las preguntas de tu encuesta. ¿Cuál es la primera pregunta?')
 
+     logger.get_logger().info(VOTING)
      return QUESTION
 
-def question(bot,update):
-#     logger.info("Description of poll: %s", update.message.text)
-     update.message.reply_text('Todo listo, ¡nos vemos!')
+def set_question(bot,update):
+     global VOTING
+     VOTING['question'] = update.message.text
+     update.message.reply_text('A continuación, indica las respuestas separadas por saltos de línea. Por ejemplo:\nRespuesta 1 \nRespuesta 2')
 
-     return ConversationHandler.END
-	
+     logger.get_logger().info(VOTING)
+     return ANSWERS
+
+def set_answers(bot,update):
+     global VOTING
+     reply_keyboard = [['Sí', 'No']]
+     options = []
+     for i in update.message.text.split("\n"):
+          options.append(i)
+
+     VOTING['opt'] = options
+     update.message.reply_text('La pregunta se ha guardado. ¿Quieres crear otra?',
+     reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+     return NEXT_QUESTION 
+
+def set_next_question(bot, update):
+     global VOTING
+     next_state = ConversationHandler.END
+
+     if(update.message.text == 'Sí'):
+          next_state = QUESTION
+          update.message.reply_text('¡De acuerdo! ¿Cuál es la nueva pregunta?')
+     else:
+          update.message.reply_text('¡Nos vemos!')
+          
+     return next_state
+     
 def cancel(bot, update):
-     user = update.message.from_user
      update.message.reply_text('La creación de la votación se ha cancelado :(')
 
      return ConversationHandler.END
 
 def main(dispatcher):
-     # start_handler = CommandHandler('poll', poll)
-     # dispatcher.add_handler(start_handler)
-     print("entered main")
      conv_handler = ConversationHandler(
-     entry_points=[CommandHandler('name', name)],
+          entry_points=[CommandHandler('poll', poll)],
 
-     states={
-          DESCRIPTION: [MessageHandler(Filters.text, description)],
+          states={
+               NAME: [MessageHandler(Filters.text, set_name)],
+              
+               DESCRIPTION: [MessageHandler(Filters.text, set_description)],
 
-          QUESTION: [MessageHandler(Filters.text, question)],
-     },
+               QUESTION: [MessageHandler(Filters.text, set_question)],
+               
+               ANSWERS: [MessageHandler(Filters.text, set_answers)],
+               
+               NEXT_QUESTION: [MessageHandler(Filters.text, set_next_question)],
+          },
 
-     fallbacks=[CommandHandler('cancel', cancel)]
+          fallbacks=[CommandHandler('cancel', cancel)]
      )
      dispatcher.add_handler(conv_handler)
