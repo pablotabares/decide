d81548378024228cd8d6e3114ca61d5a3688ed91
AlejandroMonteseirin <alexmonteseirin@gmail.com>
2018-12-11 20:57:05 +0100

    Bot now can obtain token from user and password [close #69]

--- slack-bot/slackbot.py
+++ slack-bot/slackbot.py
@@ -2,6 +2,7 @@ import os
 import time
 import re
 from slackclient import SlackClient
+import requests
 
 
 # instantiate Slack client
@@ -11,7 +12,8 @@ starterbot_id = None
 
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-EXAMPLE_COMMAND = "do"
+
+
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
 def parse_bot_commands(slack_events):
@@ -36,25 +38,52 @@ def parse_direct_mention(message_text):
     # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
+def login(usurname,password):
+    # Sends the response back to the channel
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text='logueando'
+    )
+    credentials = {}
+    credentials["username"] = usurname
+    credentials["password"] = password
+    r = requests.post("https://decide-ortosia.herokuapp.com/authentication/login/", credentials)
+
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=r
+    )
+
 def handle_command(command, channel):
     """
         Executes bot command if the command is known
     """
     # Default response is help text for the user
-    default_response = "Suspenso!. los comandos deben seguir el formato '@ortosia do [comando]', envia '@ortosia do help' para saber más!"
-
+    default_response = "No te entiendo! Los comandos de @Ortosia deben seguir un formato especifico', envia '@ortosia send help pls' para saber más!"
+    responder=True
     # Finds and executes the given command, filling in response
     response = None
     # This is where you start to implement more commands!
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Ha usted aprobado!"
 
-    # Sends the response back to the channel
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=response or default_response
-    )
+    if command.startswith('send'):
+        if command.endswith('pls'):
+            response = "Enviando ayuda! \n \n Listado de comandos: \n @ortosia send help pls, envia el mensaje de ayuda \n @ortosia logueame! [nombre de usuario] [contraseña], inicia sesión la plataforma decide\n mas cosas"
+        else:
+            response = "Es necesario pedir las cosas educadamente a @Ortosia, añada un 'pls' al final de su peticion, gracias"
+    elif command.startswith('logueame'):
+        splited=command.split(" ")
+        login(splited[len(splited)-2],splited[len(splited)-1])
+        responder=False
+
+    if (responder==True):
+        # Sends the response back to the channel
+        slack_client.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=response or default_response
+        )
 
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
@@ -68,3 +97,5 @@ if __name__ == "__main__":
             time.sleep(RTM_READ_DELAY)
     else:
         print("Connection failed. Exception traceback printed above.")
+
+
