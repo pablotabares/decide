0770669a4107631e56e1fa27673df10f996c27f0
MarioBG <Mario.biz.gon@gmail.com>
2018-12-12 16:43:02 +0100

    Updated Unity project with login capabilities
    User can now log into a locally hosted Decide instance within the Unity application.

---
+++ EGC_Decide/Assets/LoginManagerAndTeleporter.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CI.HttpClient;
+using System.Net;
+using Newtonsoft.Json;
+
+public class LoginManagerAndTeleporter : MonoBehaviour {
+
+    [SerializeField]
+    private Transform destination;
+    private HttpClient client;
+    public string token;
+    [SerializeField]
+    private Transform errorPanel;
+
+	// Use this for initialization
+	void Start () {
+		client = new HttpClient();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            commenceProcess(other.gameObject);
+        }
+    }
+
+    public void receiveLogin(HttpResponseMessage response)
+    {
+        Debug.Log("henlo");
+        try
+        {
+            Debug.Log(response.ReadAsString());
+            var values = JsonUtility.FromJson<DecideResponse>(response.ReadAsString());
+            Debug.Log(values.token);
+            this.token = values.token;
+            Debug.Log(values.non_field_errors.Count>0?values.non_field_errors[0]:"nada de nada");
+        }
+        catch (System.ArgumentNullException)
+        {
+            indicateError("El servidor no está activo en este momento");
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private void indicateError(string error)
+    {
+        StartCoroutine(indicateErrorCoroutine(error, 2));
+    }
+
+    private void indicateError(string error, float time)
+    {
+        StartCoroutine(indicateErrorCoroutine(error, time));
+    }
+
+    private IEnumerator indicateErrorCoroutine(string error, float time)
+    {
+        FindObjectOfType<FocusTogglerAndPauser>().canAlternateNormally = false;
+        FindObjectOfType<FocusTogglerAndPauser>().hasFocus = false;
+        errorPanel.gameObject.SetActive(true);
+        errorPanel.Find("Text").GetComponent<UnityEngine.UI.Text>().text = error;
+        yield return new WaitForSecondsRealtime(time);
+        errorPanel.gameObject.SetActive(false);
+        FindObjectOfType<FocusTogglerAndPauser>().canAlternateNormally = true;
+        FindObjectOfType<FocusTogglerAndPauser>().hasFocus = true;
+    }
+
+    public void makeLogin(string username, string password)
+    {
+        Debug.Log("henlo " + username + " " + password);
+        client.Post(new System.Uri("http://127.0.0.1:8000/authentication/login/"), new StringContent("{\"username\":\""+username+"\", \"password\":\""+password+"\"}",System.Text.UTF8Encoding.UTF8, "application/json"), HttpCompletionOption.AllResponseContent, receiveLogin);
+    }
+
+    private void commenceProcess(GameObject player)
+    {
+        FindObjectOfType<FocusTogglerAndPauser>().canAlternateNormally = false;
+        player.transform.position = destination.position;
+    }
+}
+
+[System.Serializable]
+public class DecideResponse
+{
+    public string token;
+    public List<string> non_field_errors;
+}
---
+++ EGC_Decide/Assets/PauseWhenHoveringOrActive.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PauseWhenHoveringOrActive : MonoBehaviour {
+
+    private GameObject focusedUIElement;
+    private GameObject selectedUIElement;
+    private TMP_InputField inputField;
+
+	// Use this for initialization
+	void Start () {
+        inputField = gameObject.GetComponent<TMP_InputField>();
+    }
+
+    public void addToFocused(GameObject g)
+    {
+        focusedUIElement = g;
+    }
+
+    public void selectThis(GameObject g)
+    {
+        selectedUIElement = g;
+    }
+
+    public void deselectThis(GameObject g)
+    {
+        selectedUIElement = null;
+    }
+
+    public void removeFromFocused(GameObject g)
+    {
+        focusedUIElement = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (focusedUIElement != null || selectedUIElement != null)
+            FindObjectOfType<FocusTogglerAndPauser>().uiSourcedPause = true;
+        else
+            FindObjectOfType<FocusTogglerAndPauser>().uiSourcedPause = false;
+	}
+}
