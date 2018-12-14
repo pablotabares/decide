5679d640339c41284942154322f0a6d89f8e5676
raffrearaUS <raffrearaus@gmail.com>
2018-12-14 09:59:46 +0100

    Included Heroku integration
    This commit is the first one related to the inclusion of Heroku integration with Travis CI. #78

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -153,6 +153,8 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
+APIS = {}
+
 try:
     from local_settings import *
 except ImportError:
@@ -160,8 +162,6 @@ except ImportError:
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
-
-APIS = {}
 
 import django_heroku
 django_heroku.settings(locals())
