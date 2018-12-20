24d48b97731f00dd6cdc28b76d76eec20f14109a
Angel Perez <angperllo@alum.us.es>
2018-12-20 15:01:55 +0100

    Updated heroku files
    Updated heroku files to try to deploy

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -153,10 +153,14 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
+APIS= {}
+
 try:
     from local_settings import *
 except ImportError:
     print("local_settings.py not found")
 
-
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
