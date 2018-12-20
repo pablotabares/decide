ef4ae6d0e2db9e334fa0480da72702d03131e46c
Angel Perez <angperllo@alum.us.es>
2018-12-20 20:02:36 +0100

    Updated settings.py
    Heroku tried to connect in localhost, so I have changed the url for the apis

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -69,7 +69,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://decide-agepere.herokuapp.com/'
+BASEURL = 'http://decide-agepere.herokuapp.com'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -154,13 +154,15 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
-APIS={}
 
 
 try:
     from local_settings import *
 except ImportError:
     print("local_settings.py not found")
+
+APIS={}
+
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
