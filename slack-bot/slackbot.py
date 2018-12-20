5955239ad2fd60fce11c9f957ad85a7a809a7c3c
AlejandroMonteseirin <alexmonteseirin@gmail.com>
2018-12-20 20:25:40 +0100

    slackbot now can create polls and close the user sesion, /help command updated [close #116]

--- slack-bot/slackbot.py
+++ slack-bot/slackbot.py
@@ -7,7 +7,7 @@ import psycopg2
 import json
 
 # instantiate Slack client
-slack_client = SlackClient('xoxb-496361367125-496064509811-40frIeS25WwFuujQwFLgCpA1')
+slack_client = SlackClient('xoxb-496361367125-496064509811-ldmk7fza7dTibjFrKsSdtT61')
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
 
@@ -40,6 +40,36 @@ def parse_direct_mention(message_text):
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 
+def create_poll(command,event):
+    splited=command.split("|")
+
+    votacion={}
+    votacion['name']=splited[1]
+    votacion['desc']=splited[2]
+    votacion['question']=splited[3]
+    opt=splited[4].split(",")
+    votacion['question_opt']=opt
+
+    conn = psycopg2.connect(dbname='d3i8n8a3vv0nst',
+            user='qzxvwbjdcmhnsy',
+            password='39cb3668dfac02f210f27e0d813167519ccf63309560bca7f93d2d79be46f308',
+            host='ec2-54-246-85-234.eu-west-1.compute.amazonaws.com',
+            port=5432
+            )
+    c = conn.cursor()
+    c.execute('SELECT token FROM userSlack where username=(%s)', [str(event['user'])])
+    response=c.fetchall()
+
+    if(len(str(response))<5):
+        return 'Es necesario estar logueado!'
+
+    token=str(response[0][0])
+    headers = {"Authorization": "Token " + token}
+
+    r = requests.post("https://decide-ortosia.herokuapp.com/voting/", votacion, headers=headers)
+    return 'todo correcto, se ha creado correctamente'
+
+
 
 def login(usurname,password,event):
     # Sends the response back to the channel
@@ -68,7 +98,7 @@ def login(usurname,password,event):
     c.execute('''CREATE TABLE IF NOT EXISTS userSlack (username text PRIMARY KEY, token text);''')
 
     # DELETE previus data a row of data
-    c.execute('DELETE FROM userSlack WHERE username=(%s)', [str(event['user'])])
+    c.execute('DELETE FROM userSlack where username=(%s)', [str(event['user'])])
     # Insert a row of data
     c.execute("INSERT INTO userSlack VALUES ("+"'"+str(event['user'])+"'"+", "+"'"+json.loads(r.text)["token"]+"'"+")")
 
@@ -95,7 +125,9 @@ def handle_command(command,event):
     responder=True
     # Finds and executes the given command, filling in response
     response = None
-    # This is where you start to implement more commands!
+    # This is  you start to implement more commands!
+    if command.startswith('crea votacion'):
+        response=create_poll(command,event)
     if command.startswith('quien soy?'):
         response='tu id de usuario es '+str(event['user'])
     if  command.startswith('estoy logueado?'):
@@ -106,18 +138,32 @@ def handle_command(command,event):
             port=5432
             )
         c = conn.cursor()
-        c.execute('SELECT token FROM userSlack WHERE username=(%s)', [str(event['user'])])
-        response=str(c.fetchall()[0][0])
-        if(len(response)<5):
+        c.execute('SELECT token FROM userSlack where username=(%s)', [str(event['user'])])
+        response=c.fetchall()
+        if(len(str(response))<5):
             response='No estas logueado aun, utiliza "@ortosia logueame [usurname] [password]" para loguearte en decide' 
         else:
-            response='Estas logueado correctamente, tu token de sesión es: '+response
+            response='Estas logueado correctamente, tu token de sesión es: '+str(response[0][0])
         conn.close()
         c.close()
 
+    if  command.startswith('cierrame la sesion'):
+        conn = psycopg2.connect(dbname='d3i8n8a3vv0nst',
+            user='qzxvwbjdcmhnsy',
+            password='39cb3668dfac02f210f27e0d813167519ccf63309560bca7f93d2d79be46f308',
+            host='ec2-54-246-85-234.eu-west-1.compute.amazonaws.com',
+            port=5432
+            )
+        c = conn.cursor()
+        c.execute('DELETE FROM userSlack where username=(%s)', [str(event['user'])])
+        conn.commit()
+        conn.close()
+        c.close()
+        response='eliminado usuario '+str(event['user'])+' de la base de datos de ortosiaBot'
+
     if command.startswith('send'):
         if command.endswith('pls'):
-            response = "Enviando ayuda! \n \n Listado de comandos: \n @ortosia send help pls, envia el mensaje de ayuda \n @ortosia logueame! [nombre de usuario] [contraseña], inicia sesión la plataforma decide\n \n Comandos avanzados(desarrollo): \n @ortosia quien soy?: devuelve el id de usuario \n @ortosia estoy logueado? devuelve el token de autorización de decide en caso estar logueado"
+            response = "Enviando ayuda! \n \n Listado de comandos: \n @ortosia send help pls, envia el mensaje de ayuda \n @ortosia logueame! [nombre de usuario] [contraseña], inicia sesión la plataforma decide\n@ortosia crea votacion | [titulo] | [descripcion] | [pregunta] | [opciones separadas por comas]: crea la votacion en la plataforma decide, es necesario estar logueado correctamente en la plataforma decide y tener los permisos necesarios \n\n Comandos avanzados(desarrollo): \n @ortosia quien soy?: devuelve el id de usuario \n @ortosia estoy logueado? devuelve el token de autorización de decide en caso estar logueado\n @ortosia cierrame la sesion : cierra la sesion del usuario"
         else:
             response = "Es necesario pedir las cosas educadamente a @Ortosia, añada un 'pls' al final de su peticion, gracias"
     elif command.startswith('logueame'):
