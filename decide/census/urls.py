9b63513afd1fb0903bed8233a8014866f5a177a4
manheravi <manheravi@alum.us.es>
2018-12-20 17:28:51 +0100

    Add list of voters of a given census
    Create the view for list of all voters and votings and the list of voters of a voting giving his id. In addition added the logic for those views

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
+    path('list', views.CensuslListView.as_view(), name='census_list'),
+    path('list/<int:voting_id>/', views.CensuslByVotingListView.as_view(), name='census_by_voting_list'),
 ]
--- decide/census/views.py
+++ decide/census/views.py
@@ -1,7 +1,10 @@
 from django.db.utils import IntegrityError
+from django.shortcuts import redirect
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.response import Response
+from django.views.generic.list import ListView
 from rest_framework.status import (
         HTTP_201_CREATED as ST_201,
         HTTP_204_NO_CONTENT as ST_204,
@@ -12,6 +15,38 @@ from rest_framework.status import (
 
 from base.perms import UserIsStaff
 from .models import Census
+from voting.models import Voting
+
+class CensuslListView(ListView):
+    model = Census
+    template_name = "list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_list'] = Census.objects.all()
+        return context
+
+class CensuslByVotingListView(ListView):
+    model = Census
+    template_name = "list.html"
+    votingId = 0
+    voting = None
+    
+    def get(self, request, *args, **kwargs):
+        self.votingId = kwargs.get('voting_id')
+        try:
+            self.voting = Voting.objects.get(pk=self.votingId)
+        except ObjectDoesNotExist as n:
+            return redirect('census_list')
+        return ListView.get(self, request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ids = Census.objects.filter(voting_id=self.votingId).values('voter_id')
+        context['object_list'] = User.objects.all().filter(pk__in=ids)
+        context['voting'] = self.voting
+        print(kwargs)
+        return context
 
 
 class CensusCreate(generics.ListCreateAPIView):
