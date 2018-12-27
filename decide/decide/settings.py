44a1f0af88392b96bca6b52c3cca8df7401cfb78
Camila Reyes <cami_jara@hotmail.es>
2018-12-27 17:30:02 +0100

    missing import added django-heroku import added [#117]

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-<<<<<<< HEAD
 import django_heroku
 import dj_database_url
-=======
->>>>>>> 1a14c693ba89eec3b93ea073607d234c61012528
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
