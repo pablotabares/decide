cf7106cdb502d9afdea0926fa11133744baf5975
manheravi <manheravi@alum.us.es>
2019-01-01 13:56:51 +0100

    Booth 2 - API link for fetching votes of an user #111
    Added an API method on Census views solving the above issue

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
     path('list', views.CensuslListView.as_view(), name='census_list'),
     path('list/<int:voting_id>/', views.CensuslByVotingListView.as_view(), name='census_by_voting_list'),
+    path('voter/<int:voter_id>/', views.CensusList.as_view(), name="votings_by_voter"),
 ]
