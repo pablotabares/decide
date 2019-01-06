4391300b36ce8ea5684ac0bc76d092804e4837c2
Wordsan <esagmo96@gmail.com>
2019-01-06 19:09:17 +0100

    Urls problem

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
     path('list', views.CensuslListView.as_view(), name='census_list'),
     path('list/<int:voting_id>/', views.CensuslByVotingListView.as_view(), name='census_by_voting_list'),
-    path('list/<str:startDate>&&<str:endDate>/', views.VotingByDateListView.as_view(), name='voting_by_date_list')
+    path('list/<str:startDate>&&<str:endDate>/', views.VotingByDateListView.as_view(), name='voting_by_date_list'),
     path('voter/<int:voter_id>/', views.CensusList.as_view(), name="votings_by_voter"),
 ]
