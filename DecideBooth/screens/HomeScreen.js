0b00d4ecb976b3c16a8dbbb686437169ede95877
César García <cgpcsjcmp@gmail.com>
2019-01-07 23:16:49 +0100

    ADD: Home Screen displaying votes and onPress action
    Pressing on a vote (Only finished votings are shown) triggers an alert window.

--- DecideBooth/screens/HomeScreen.js
+++ DecideBooth/screens/HomeScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {
-  Image,
+  Image,AsyncStorage, StyleSheet, Alert
 } from 'react-native';
-import {Container, Header, Content, Button,Card, CardItem, Icon, Accordion,Body, Title,Text, Left, Right } from "native-base";
+import {Container, Header, Content, Button,Card, CardItem, Icon, List, ListItem, Accordion,Body, Title,Text, Left, Right } from "native-base";
 import {NavigationActions} from "react-navigation";
+import moment from "moment";
 
 const dataArray = [
 { title: "My votings", content:"AAA" },
@@ -16,11 +17,43 @@ export default class HomeScreen extends React.Component {
     header: null,
   };
 
-  constructor(props){
-    super(props);
+  constructor(props) {
+      super(props);
+      this.state = {
+          votings : []
+      }
   }
 
 
+  _getVotings = async() => {
+      const token = await AsyncStorage.getItem('userToken');
+      const id = await AsyncStorage.getItem('userId');
+      if(id !== null && token !== null) {
+          fetch('http://decide-ortosia.herokuapp.com/census/voter/'+id, {
+              method: 'GET',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+              }
+          })
+              .then(response => {
+                  let votings = JSON.parse(response._bodyText);
+                  this.setState({
+                      votings: votings
+                  });
+              })
+              .catch(error => console.error(error));
+      }else{
+          this._signOutAsync();
+      }
+  };
+
+
+
+
+  componentWillMount() {
+      this._getVotings();
+  }
 
   render() {
     // const {navigate} = this.props.navigation;
@@ -46,11 +79,43 @@ export default class HomeScreen extends React.Component {
                 </CardItem>
 
                 <CardItem>
-                    <Accordion dataArray={dataArray} />
+                  <List dataArray={this.state.votings}
+                        renderRow={(item) =>
+                            <VoteItem item={item} />
+                        }>
+                  </List>
                 </CardItem>
             </Card>
           </Content>
         </Container>
     );
   }
+}
+
+
+class VoteItem extends React.Component{
+
+  _showVoting(voting_id) {
+    Alert.alert('EEEEEE'+voting_id)
+  }
+
+    render(){
+        if(moment() > moment(this.props.item.end_date,'YYYY-MM-DDTHH:mm:ss.SSSSSSZ')){
+          return(
+              <ListItem noIndent onPress={() =>this._showVoting(this.props.item.id)} style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+              }}>
+                  <Left>
+                      <Icon name='check' type='MaterialIcons' style={{color: 'green'}}/>
+                  </Left>
+                  <Body>
+                  <Text>{this.props.item.name}</Text>
+                  <Text note>{this.props.item.desc}</Text>
+                  </Body>
+              </ListItem>
+          )
+        } else {return null}
+    }
 }
