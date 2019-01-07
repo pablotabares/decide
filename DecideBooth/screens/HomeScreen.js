912423a96250e1244ef7e1cb1d79cbef1a671a81
César García <cgpcsjcmp@gmail.com>
2019-01-07 22:39:27 +0100

    ADD: Homescreen displaying image and accordion
    Home screen now displays an image and an acordion for different votings.

--- DecideBooth/screens/HomeScreen.js
+++ DecideBooth/screens/HomeScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {
-  Text,
+  Image,
 } from 'react-native';
-import {Container, Content, Header} from "native-base";
+import {Container, Header, Content, Button,Card, CardItem, Icon, Accordion,Body, Title,Text, Left, Right } from "native-base";
 import {NavigationActions} from "react-navigation";
+
+const dataArray = [
+{ title: "My votings", content:"AAA" },
+{ title: "Popular votings", content: "Lorem ipsum dolor sit amet" }
+];
+
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -14,15 +20,35 @@ export default class HomeScreen extends React.Component {
     super(props);
   }
 
+
+
   render() {
     // const {navigate} = this.props.navigation;
     // console.log(typeof this.token);
     // if(this.navigation.state.token === undefined) navigate('Login');
     return (
         <Container>
-          <Header/>
+          <Header>
+            <Title>Home</Title>
+          </Header>
           <Content>
-            <Text>Home Screen: </Text>
+            <Card >
+
+                <CardItem>
+                    <Text style={{fontWeight: 'bold'}}>Welcome to Decide</Text>
+                </CardItem>
+
+                <CardItem header bordered >
+                  <Image
+                    style={{width: 322,height: 60}}
+                    source={require('../img/banner.jpeg')}
+                  />
+                </CardItem>
+
+                <CardItem>
+                    <Accordion dataArray={dataArray} />
+                </CardItem>
+            </Card>
           </Content>
         </Container>
     );
