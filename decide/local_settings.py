201e65cd825c607499ecac36dadf032a92eb49f5
Pablo Tabares García <pablo.tabaresgarcia@gmail.com>
2019-01-09 22:33:09 +0100

    Modify: Trying out a 4 worker gunicorn

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -1,3 +1,5 @@
+import urllib.parse as urlparse
+
 ALLOWED_HOSTS = ["*"]
 
 # Modules in use, commented modules that you won't use
@@ -14,29 +16,48 @@ MODULES = [
 ]
 
 APIS = {
-    'authentication': 'localhost:8000',
-    'base': 'localhost:8000',
-    'booth': 'localhost:8000',
-    'census': 'localhost:8000',
-    'mixnet': 'localhost:8000',
-    'postproc': 'localhost:8000',
-    'store': 'localhost:8000',
-    'visualizer': 'localhost:8000',
-    'voting': 'localhost:8000',
+    'authentication': 'http://localhost:8000',
+    'base': 'http://localhost:8000',
+    'booth': 'http://localhost:8000',
+    'census': 'http://localhost:8000',
+    'mixnet': 'http://localhost:8000',
+    'postproc': 'http://localhost:8000',
+    'store': 'http://localhost:8000',
+    'visualizer': 'http://localhost:8000',
+    'voting': 'http://localhost:8000',
 }
 
-BASEURL = 'localhost:8000'
+BASEURL = 'http://localhost:8000'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'decide',
+#         'PASSWORD': 'decide',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
+url = urlparse.urlparse('postgres://mgvbzsuwamkstu:b0295daff88b2758623026d0b121906d8ab6b0ee28e120b334a42284d8ef69f4@ec2-54-75-245-94.eu-west-1.compute.amazonaws.com:5432/d5q0v8pfqn73ec')
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'decide',
-        'PASSWORD': 'decide',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': dbname,
+        'USER': user,
+	    'PASSWORD': password,
+        'HOST': host,
+        'PORT': port,
     }
 }
+
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
