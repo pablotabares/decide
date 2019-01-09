a47db9db0c3be6f142380e8750520734317f51c4
Pablo Tabares García <pablo.tabaresgarcia@gmail.com>
2019-01-09 01:03:56 +0100

    Modify: Trying to fix BROKER_URL not loading properly

--- decide/decide/celery.py
+++ decide/decide/celery.py
@@ -13,7 +13,7 @@ app = Celery('decide', BROKER_URL=settings.REDIS_URL,
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
