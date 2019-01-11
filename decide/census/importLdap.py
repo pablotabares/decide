d4c2a804ca46f8b317da9c96ad4e7de9f5817ce0
Ferkaj <f_javi_fm@hotmail.com>
2019-01-11 17:54:00 +0100

    Completed function #115

--- decide/census/importLdap.py
+++ decide/census/importLdap.py
@@ -1,21 +1,34 @@
 import ldap
 
-ldap_server="ldap://localhost:389"
-username = "admin"
-password= "admin"
-# the following is the user_dn format provided by the ldap server
-user_dn = "cn="+username+",dc=example,dc=org"
-# adjust this to your base dn for searching
-base_dn = "dc=example,dc=org"
-connect = ldap.initialize(ldap_server)
-search_filter = "cn="+username
-try:
-    #if authentication successful, get the full user data
-    connect.bind_s(user_dn,password)
-    result = connect.search_s(base_dn,ldap.SCOPE_SUBTREE,search_filter)
-    # return all user data results
-    connect.unbind_s()
-    print (result)
-except ldap.LDAPError:
-    connect.unbind_s()
-    print( "authentication error")
+
+# Funcion para importar usuarios de ldap,
+# return lista de nombres
+def importLdap(ldap_server,username,password,base_dn):
+
+
+    connect = ldap.initialize(ldap_server)
+    #Filtro para buscar objectos de tipo persona
+    search_filter = "objectClass=inetOrgPerson"
+
+    try:
+        connect.bind_s(user_dn,password)
+        result = connect.search_s(base_dn,ldap.SCOPE_SUBTREE,search_filter)
+        listPerson=[]
+        for x in range(0,len(result)):
+            listPerson.append(result[x][1]['cn'][0].decode("utf-8"))
+        connect.unbind_s()
+        return listPerson
+    except ldap.LDAPError:
+        connect.unbind_s()
+        print( "Ldap error")
+
+
+# #Datos de control
+# ldap_server="ldap://ldap.forumsys.com:389"
+# username = "read-only-admin"
+# password= "password"
+# base_dn = "dc=example,dc=com"
+# user_dn = "cn="+username+","+base_dn
+# lista=importLdap(ldap_server,username,password,base_dn)
+# print(lista)
+
