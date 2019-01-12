0fb23cf2411b9fd8933efa34dbf88024961d264d
César García <cgpcsjcmp@gmail.com>
2019-01-12 17:34:28 +0100

    FIX: Bug in Visualizer Urls.py
    BUG: NameError: name 'views' is not defined
    The command "python manage.py makemigrations" failed and exited with 1 during .
    FIX: Deleted views because it was imported already.

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -4,5 +4,5 @@ from .views import VisualizerView
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
-    path('json/<int:voting_id>/', views.VisualizerJSON.as_view()),
+    path('json/<int:voting_id>/', VisualizerJSON.as_view()),
 ]
