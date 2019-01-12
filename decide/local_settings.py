0a3c33bafcf0707c7410d4067834ce3ad9a863e6
Camila Reyes <cami_jara@hotmail.es>
2019-01-12 18:18:55 +0100

    Telegram bot removed Telegram bot removed from project, moved to https://github.com/ortosia-cabina/telegram-bot [close pablotabares/decide#126]

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -30,8 +30,9 @@ BASEURL = 'http://localhost:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': 'decide',
+        'USER': 'decide',
+        'PASSWORD': 'decide', 
         'HOST': 'localhost',
         'PORT': '5432',
     }
