858bed0fa486b6bd485da50a9d07f31b6e20d667
adracohid <adracohid@alum.us.es>
2019-01-13 12:10:19 +0100

    .

--- decide/mixnet/urls.py
+++ decide/mixnet/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('shuffle/<int:voting_id>/', views.Shuffle.as_view(), name='shuffle'),
     path('decrypt/<int:voting_id>/', views.Decrypt.as_view(), name='decrypt'),
     path('form_zkp', views.fs_form, name='zkp'),
-    path('result', views.Result.as_view(), name='res')
+    path('result', views.Result.as_view(), name='res'),
     path('populate', views.Populate.as_view()),
 
 ]
